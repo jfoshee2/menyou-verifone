40e79f4d0cad5275bf4decd0cb00a1e8165062f9
austinford <austin.ford1311@gmail.com>
2018-04-24 14:51:19 -0400

    Fixed the Keyboard opening when you view an item

--- app/src/main/java/com/example/james/menyou_verifone/item/MenuItemDetailActivity.java
+++ app/src/main/java/com/example/james/menyou_verifone/item/MenuItemDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -39,10 +40,9 @@ public class MenuItemDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_detail);
-
         ApplicationComponent applicationComponent = DaggerApplicationComponent.builder().build();
         MenuItemRestAdapter menuItemRestAdapter = applicationComponent.getMenuItemRestAdapter();
-
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         main = new Intent(this, MainActivity.class);
         orderActivity = new Intent(this, MainOrderActivity.class);
 
